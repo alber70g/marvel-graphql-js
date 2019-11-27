5ba7f4d674df2c6437dd19dc0c2f4889ac63db64
Albert Groothedde <albertgroothedde@gmail.com>
2019-11-27 16:55:09 +0100

    Solution 5

--- graphql/resolvers/character.js
+++ graphql/resolvers/character.js
@@ -3,9 +3,6 @@ import logger from '../../core/logger/app-logger';
 
 const resolveFunctions = {
   Character: {
-    thumbnail({ thumbnail: { path, extension } }) {
-      return `${path}.${extension}`;
-    },
     comics(parent) {
       return parent.comics.items;
     },
